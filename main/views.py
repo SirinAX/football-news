a273ddcb1bec13ae3ed4fb5af17e0310d38ec47d
Andi <hakimandi976@gmail.com>
2025-09-03 14:56:52 +0700

    Complete tutorial 1: Django MVT implementation(after revision)

--- main/views.py
+++ main/views.py
@@ -5,7 +5,7 @@ def show_main(request):
     context = {
         'npm' : '2406495792',
         'name': 'Andi Hakim Himawan',
-        'class': 'PBP-D'
+        'kelas': 'PBP-D'
     }
 
     return render(request, "main.html", context)
