e2cad49725a99fb4c82b2a0668d2c09a99ac5357
unknown <hakimandi976@gmail.com>
2025-10-01 13:54:53 +0700

    Tutorial 5 Dones

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.forms import ModelForm
 from main.models import News
+from django.utils.html import strip_tags
 
 class NewsForm(ModelForm):
     class Meta:
@@ -17,10 +18,17 @@ class NewsForm(ModelForm):
             "category": forms.Select(attrs={
                 "class": "border rounded-md p-2 w-full"
             }),
-            "thumbnail": forms.ClearableFileInput(attrs={
+            "thumbnail": forms.TextInput(attrs={
                 "class": "border rounded-md p-2 w-full"
             }),
             "is_featured": forms.CheckboxInput(attrs={
                 "class": "mr-2"
             }),
         }
+    def clean_title(self):
+        title = self.cleaned_data["title"]
+        return strip_tags(title)
+
+    def clean_content(self):
+        content = self.cleaned_data["content"]
+        return strip_tags(content)
